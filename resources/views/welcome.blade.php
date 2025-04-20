0e2eac04423cea9a07470ba43d1af771c5baf63c
Matthijs Veldkamp <99071402@mydavinci.nl>
2025-04-20 23:55:51 +0200

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,5 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="hero min-h-[calc(100vh-4rem)] bg-base-200">
-    <div class="hero-content text-center">
-        <div class="max-w-md">
-            <h1 class="text-5xl font-bold">Minecraft Dashboard</h1>
-            <p class="py-6">Monitor your Minecraft server in real-time. Track players, performance, and manage your server all from one place.</p>
-            <div class="flex gap-4 justify-center">
-                <a href="#" class="btn btn-primary">Get Started</a>
-                <a href="#" class="btn btn-ghost">Learn More</a>
-            </div>
-        </div>
-    </div>
-</div>
+<a href="{{ route('dashboard1') }}" class="text-2xl font-bold hover:text-blue-600">Dashboard1</a>
 @endsection
